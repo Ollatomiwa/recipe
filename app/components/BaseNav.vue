bccdba3c9fe1f4dd08171ba44ba85af05273b40c
ollatomiwa <ajaomoshood@gmail.com>
2025-02-14 02:19:05 +0100

    file structured and pages setup

---
+++ app/components/BaseNav.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <header class="py-4 border-6">
+        <nav class="flex container items-center">
+            <a href="" class="flex gap-1 items-center">
+                <img src="" class="w-14" alt="">
+                <span class="text-3xl font-bold">Nuxtclipes</span>
+            </a>
+            <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link> 
+                </li>
+                <li>
+                    <nuxt-link to="/about">About</nuxt-link>>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+
+
