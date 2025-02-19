c9a225f75fced164602ae9272017b848b8975647
ollatomiwa <ajaomoshood@gmail.com>
2025-02-19 10:57:18 +0100

    update to sm screen in about

--- app/components/About.vue
+++ app/components/About.vue
@@ -6,7 +6,7 @@
 <template>
    <main>
     <section class="container py-28" id="about">
-        <h1 class="text-6xl font-extrabold mb-6 text-balance">About Computcipes</h1>
+        <h1 class="text-4xl sm:text-[40px] font-extrabold mb-6 text-balance">About Computcipes</h1>
         <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
             Comput Recipe application is based on the dummy API, created by the world renowed chef <hr /> <span class=" text-3xl font-bold text-dodgeroll-gold">
                 Make delicious meals with Comput Recipe course.
